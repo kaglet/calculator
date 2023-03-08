793360c11ef3c60b5de4b8d1a1e62341cb98fcb3
Kago <kmotlhabane769@gmail.com>
2023-03-08 21:08:45 +0200

    Make basic calculator without good flex layout

---
+++ script.js
@@ -0,0 +1,43 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function operate(operator, num1, num2) {
+    let result = undefined;
+    switch (operator) {
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = subtract(num1, num2);
+            break;
+        case "*":
+            result = multiply(num1, num2);
+            break;
+        case "/":
+            result = divide(num1, num2);
+            break;
+        default:
+            break;
+    }
+
+    return result;
+}
+
+function setupPage() {
+
+}
+
+window.addEventListener('load', setupPage);
